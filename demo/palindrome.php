d46c4d30d5126aa0832253e9c9f1e61f4387ebd3
Moonhee Kim <134743009+Moonheeui@users.noreply.github.com>
2023-10-31 17:23:00 -0400

    Update palindrome.php

--- demo/palindrome.php
+++ demo/palindrome.php
@@ -27,18 +27,18 @@ for ($i = 0; $i < strlen($phaseToTest); $i++) {
     // if ($phaseToTest[$i] != " " && $phaseToTest[$i] != ".") {
     //     $forwards .= $phaseToTest[$i];
 
-    if (!($phaseToTest[$i] != " " || $phaseToTest[$i] != ".")) {
+    if (!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
         $forwards .= $phaseToTest[$i];
     }
 }
-// echo $forwards;
+echo $forwards, "front";
 
-for ($i = strlen($phaseToTest) - 1; $i >= 0; $i++) {
+for ($i = strlen($phaseToTest) - 1; $i >= 0; $i--) {
     if (!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
         $backwards .= $phaseToTest[$i];
     }
 }
-// echo $backwards;
+echo $backwards, "back";
 
 
 // compare the 2 new variables
@@ -47,19 +47,4 @@ if ($forwards == $backwards) {
 }
 
 
-// compare from the front AND from the back simultaneously
-// keep track of my front index
-// keep track of my back index SEPARATELY
-// $iFront = 0;
-$backIndex = strlen($phaseToTest) - 1;
-
-for ($i = 0; $i < strlen($phaseToTest); $i++) {
-
-    // while I have a space or a period, skip to the next index
-    while ($phaseToTest[$i] == " " || $phaseToTest[$i] == ".") {
-        $i++;
-    }
-
-    // echo $backIndex;
-    $backIndex--;
-}
+?>
