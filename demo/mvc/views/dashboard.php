733598d6684bbe3cf10ec57bfe350d995c4f8cde
Moonhee Kim <134743009+Moonheeui@users.noreply.github.com>
2023-11-15 16:19:34 -0500

    change 2 files

---
+++ demo/mvc/views/dashboard.php
@@ -0,0 +1,7 @@
+<h1>Dashboard</h1>
+
+<section>
+    <article>Dog - 2</article>
+    <article>Cat - 1</article>
+    <article>Mouse - 3</article>
+</section>
--- demo/mvc/views/logout.php
+++ demo/mvc/views/logout.php
@@ -1,1 +1,1 @@
-Log-out
+<h1>Log-out</h1>
