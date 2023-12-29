ab70acb1f0d0e78b161293dba021fb6fc0e77899
Moonheeui <2295442@edu.vaniercollege.qc.ca>
2023-12-28 23:14:43 -0500

    Change contents

--- exercises/objects-exercise.php
+++ exercises/objects-exercise.php
@@ -1,25 +1,50 @@
 <?php
 
+ini_set('display_errors', 1);
+
 class DeskLamp
 {
-
+    public $name;
+    public $size;
+    public $color;
+    public $finish;
+    public $cableLength = "1.5m";
     private $switch1;
     private $switch2 = 0;
-    private $isActive = false;
+    private $isActive = false; // the lamp on.
 
-    public function __construct()
+    public function __construct($name, $size, $color, $finish, $cableLength)
     {
-        $this->switch1 = "not active";
+        $this->name = $name;
+        $this->size = $size;
+        $this->color = $color;
+        $this->finish = $finish;
     }
 
+    public function getCableLength()
+    {
+        return $this->cableLength;
+    }
+    public function setCableLength($cableLength)
+    {
+        $this->cableLength = $cableLength;
+    }
+    public function cableConnect()
+    {
+        if ($this->cableLength >= "1.5m") {
+            echo "connected";
+        } else {
+            $this->cableLength < "1.5m";
+            echo "not connected";
+        }
+    }
     private function getSwitch1()
     {
         return $this->switch1;
     }
-
-    private function setSwitch1($turnOn)
+    private function setSwitch1($isActive)
     {
-        $this->switch1 = $turnOn;
+        $this->switch1 = $isActive;
     }
     public function ask()
     {
@@ -35,14 +60,17 @@ class DeskLamp
     }
 }
 
-$lamp1 = new DeskLamp();
-$lamp1->action("active");
-echo $lamp1->ask();
-echo $lamp1->isActive() ? "active" : "not active";
+$deskLamp = new DeskLamp("Desk Lamp", "small", "white", "matte", "1.5m");
 
+echo "<h1>Desk Object</h1>";
 
-
-// private $angle;
-// private $colorTemp;
-// private $light;
-// private $mode;
+echo "Name: " . $deskLamp->name . "</br>";
+echo "Size: " . $deskLamp->size . "</br>";
+echo "Color: " . $deskLamp->color . "</br>";
+echo "Finish: " . $deskLamp->finish . "</br>";
+echo "Cable Length: " . $deskLamp->cableLength . "</br>";
+echo $deskLamp->cableConnect() . "</br>";
+echo "</br>";
+$deskLamp->action("active");
+echo $deskLamp->ask();
+echo "</br> Swith Status: ", $deskLamp->isActive() ? "active" : "not active";
